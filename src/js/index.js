149f9f10cc1a3eeb332de5ed69b994975956c4a7
Alejandro Sanchez <aalejo@gmail.com>
2021-12-03 20:46:10 +0000

    trying to fix boilerplate

--- src/js/index.js
+++ src/js/index.js
@@ -2,11 +2,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-//include bootstrap npm library into the bundle
-import "bootstrap";
-
-//include your index.scss file into the bundle
-import "../styles/index.scss";
+// include your styles into the webpack bundle
+import "../styles/index.css";
 
 //import your own components
 import Home from "./component/home.jsx";
--- webpack.config.js
+++ webpack.config.js
@@ -28,12 +28,10 @@ module.exports = {
           use: ['babel-loader', 'eslint-loader']
         },
         {
-          test: /\.(css|scss)$/, use: [{
+          test: /\.(css)$/, use: [{
               loader: "style-loader" // creates style nodes from JS strings
           }, {
               loader: "css-loader" // translates CSS into CommonJS
-          }, {
-              loader: "sass-loader" // compiles Sass to CSS
           }]
         }, //css only files
         { 
